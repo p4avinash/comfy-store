458a6401d789ce864fde431be737dd12781baf67
p4avinash <p4avinashkumar@gmail.com>
2023-10-02 22:19:19 +0530

    added about page and Hero component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,25 @@
 const About = () => {
-  return <h1>About</h1>
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, vero
+        corporis eos nisi voluptatum error quasi voluptates voluptatem qui
+        commodi maxime atque quam hic, aut alias beatae ipsam accusamus quis!
+      </p>
+    </>
+  )
 }
 
 export default About
