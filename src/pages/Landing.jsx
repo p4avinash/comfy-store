fdcfa85e07133bd7d06746c4cb05433736195e4f
p4avinash <p4avinashkumar@gmail.com>
2023-10-02 23:25:30 +0530

    added featured products

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,20 @@
 import React from "react"
-import { Hero } from "../components"
+import { FeaturedProducts, Hero } from "../components"
+import { customFetch } from "../utils"
+
+const url = "/products?featured=true"
+
+export const loader = async () => {
+  const response = await customFetch(url)
+  const products = response.data.data
+  return { products }
+}
 
 const Landing = () => {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   )
 }
