e2d571d186a303397243759aeaf9a4d1330bc06b
p4avinash <p4avinashkumar@gmail.com>
2023-10-05 12:18:41 +0530

    added login, register and guest login funtionality

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,10 @@ import { loader as singleProductLoader } from "./pages/SingleProduct"
 import { loader as productsLoader } from "./pages/Products"
 
 //actions
+import { action as registerAction } from "./pages/Register"
+import { action as loginAction } from "./pages/Login"
+
+import { store } from "./store"
 
 const router = createBrowserRouter([
   {
@@ -69,11 +73,13 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
     errorElement: <Error />,
+    action: loginAction(store),
   },
   {
     path: "/register",
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ])
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,48 @@
-import React from "react"
 import { FormInput, SubmitBtn } from "../components"
-import { Form, Link } from "react-router-dom"
+import { Form, Link, redirect, useNavigate } from "react-router-dom"
+import { customFetch } from "../utils"
+import { toast } from "react-toastify"
+import { loginUser } from "../features/user/userSlice"
+import { useDispatch } from "react-redux"
+
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData)
+
+    try {
+      const response = await customFetch.post("/auth/local", data)
+      store.dispatch(loginUser(response.data))
+      toast.success("Logged in successfully")
+      return redirect("/")
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message || "please check your credentials"
+      toast.error(errorMessage)
+      return null
+    }
+  }
 
 const Login = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post("/auth/local", {
+        identifier: "test@test.com",
+        password: "secret",
+      })
+      dispatch(loginUser(response.data))
+      toast.success("Welcome guest user")
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+      toast.error("Guest user login error, please try again..!!")
+    }
+  }
+
   return (
     <section className='h-screen grid place-items-center'>
       <Form
@@ -11,25 +51,19 @@ const Login = () => {
       >
         <h4 className='text-center text-3xl font-bold'>Login</h4>
 
-        <FormInput
-          type='email'
-          label='email'
-          name='identifier'
-          defaultValue='test@test.com'
-        />
+        <FormInput type='email' label='email' name='identifier' />
 
-        <FormInput
-          type='password'
-          label='password'
-          name='password'
-          defaultValue='secret'
-        />
+        <FormInput type='password' label='password' name='password' />
 
         <div className='mt-4'>
           <SubmitBtn text='login' />
         </div>
 
-        <button type='button' className='btn btn-secondary btn-block'>
+        <button
+          type='button'
+          className='btn btn-secondary btn-block'
+          onClick={loginAsGuestUser}
+        >
           Guest user
         </button>
         <p className='text-center'>
